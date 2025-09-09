91298323754bf9aa7c6426bd6dc5ad32a4337f81
user <dyubkovadasa@gmail.com>
2025-09-09 14:00:36 +0300

    text
    Добавлена обработка ошибок ввода. Fixes #1

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+
+
 print('Hello World!')
 
 class Student:
-    print("Hi")
+    print("привет")
 
 def new_greeting():
     return "Добро пожаловать в наше приложение!"
 print(new_greeting())
+
+try:
+    number = int(input("Введите число: "))
+    print(f"Вы ввели: {number}")
+except ValueError:
+    print("Ошибка: введите число")
